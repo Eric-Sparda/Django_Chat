189ce438f1644bf6792c97cbfebba3f06c7b7db4
Eric-Sparda <roy.sec@bk.ru>
2023-04-25 02:12:29 +0400

    Real-Time_Chat_With_Django_Python

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -13,7 +13,7 @@ class MyUser(models.Model):
     
 class ChatMessage(models.Model):
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     message = models.TextField(max_length=3000)
     message_html = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
