6ab78635ade1b089dec293046395e27a85771826
Eric-Sparda <roy.sec@bk.ru>
2023-04-29 01:29:18 +0400

    Real-Time_Chat_With_Django_Python

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -2,10 +2,9 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
-urlpatterns=[
+urlpatterns = [
     path('', views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='main/home.html'), name='logout'),
-    path('signup/', views.signup, name='signup'),
-    path('recover_message/', views.recover_message, name='recover_message')
+    path('signup/', views.signup, name='signup')
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login
 from .models import RecoveryMessage
 from .forms import MessageForm, SignupForm
-from django.contrib.auth.forms import AuthenticationForm
-
-
-from django.shortcuts import render
 
 @login_required
 def home(request):
@@ -16,25 +11,12 @@ def home(request):
             recovery_message = form.save(commit=False)
             recovery_message.user = request.user
             recovery_message.save()
+            form = MessageForm()
+            return redirect('home')
     else:
         form = MessageForm()
-    
-    messages = RecoveryMessage.objects.filter(user=request.user)
+    messages = RecoveryMessage.objects.all().order_by('-created_at')[:10][::-1]
     return render(request, 'main/home.html', {'form': form, 'recovery_messages': messages})
-
-@login_required
-def recover_message(request):
-    if request.method == 'POST':
-        form = MessageForm(request.POST)
-        if form.is_valid():
-            recovery_message = form.save(commit=False)
-            recovery_message.user = request.user
-            recovery_message.save()
-            messages = RecoveryMessage.objects.filter(user=request.user)
-            return render(request, 'home.html', {'recovery_messages': messages})
-    else:
-        messages = RecoveryMessage.objects.filter(user=request.user)
-        return render(request, 'home.html', {'recovery_messages': messages})
 
 def signup(request):
     if request.method == 'POST':
